9ba5d8acdc143d3f918ca65a4817628697916ded
Pegah Poshtareh <poshtareh@munch2.local>
2015-11-10 15:50:19 +0100

    logmap is done.

--- logmap.cxx
+++ logmap.cxx
@@ -1,23 +1,31 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main(){
-	double x0 = 0.5;
-	double x;
+int main()
+{
+	
+	int N = 4000;
+	double x[N];
+	
+	
 	int Nskip = 100; //Number of iterations to skip
 	int Nend  = 200; //Number of total iterations
+	
+	 ofstream out("output.dat");
 
 	for(double r=0; r <= 4; r += 0.001){
-	   x=x0;
+	   x[1] = 0.5;
 	   for(int i=1; i <= Nskip; i++)
-		   x = r*x*(1-x);
+		   x[i+1] = r*x[i]*(1-x[i]);
 	   for(int i=Nskip+1; i <= Nend; i++){
-	   		   x = r*x*(1-x);
-	   		   cout << r << "\t" << x << endl;
+	   		   x[i+1] = r*x[i]*(1-x[i]);
+	   		   out << r << "\t" << x[i+1] << endl;
    	   }
 	}
-
+	
+	out.close();
 
 	return 0;
 }
